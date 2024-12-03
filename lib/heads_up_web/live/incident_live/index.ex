0363ebcbddeef60142cddd8346329f94f7fc527f
Harsh Kedia <hkedia@hey.com>
2024-12-03 21:00:45 +0530

    handling url filter params

--- lib/heads_up_web/live/incident_live/index.ex
+++ lib/heads_up_web/live/incident_live/index.ex
@@ -4,13 +4,17 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   alias HeadsUp.Incidents
 
   def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
     socket =
       socket
       |> assign(:page_title, "Incidents")
-      |> stream(:incidents, Incidents.list_incidents())
-      |> assign(:form, to_form(%{}))
+      |> stream(:incidents, Incidents.filter_incidents(params))
+      |> assign(:form, to_form(params))
 
-    {:ok, socket}
+    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -57,6 +61,9 @@ defmodule HeadsUpWeb.IncidentLive.Index do
           "Priority: Low to High": "priority_desc"
         ]}
       />
+      <.link navigate={~p"/incidents"}>
+        Reset
+      </.link>
     </.form>
     """
   end
@@ -82,10 +89,12 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   end
 
   def handle_event("filter", params, socket) do
-    socket =
-      socket
-      |> assign(:form, to_form(params))
-      |> stream(:incidents, Incidents.filter_incidents(params), reset: true)
+    params =
+      params
+      |> Map.take(~w(q status sort_by))
+      |> Map.reject(fn {_, v} -> v == "" end)
+
+    socket = push_navigate(socket, to: ~p"/incidents/?#{params}")
 
     {:noreply, socket}
   end
